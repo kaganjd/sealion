e118725d12beb5bb110a0a01e3af47b16022d68b
Jen Kagan <kaganjd@gmail.com>
2018-09-15 19:33:53 -0400

    Create Network class for client side and add to Webpack config
    - add to Webpack conf output.library and output.libraryTarget
    - add Network class with some socket methods
    - change sketch.js to use new lib and methods

--- p5.packets-client/dist/sketch.js
+++ p5.packets-client/dist/sketch.js
@@ -1,6 +1,14 @@
+let packet;
+const hostname = "localhost";
+const port = 8080;
+
 function setup() {
   createCanvas(400, 400);
   background(100, 255, 255);
+  packet = new Sniffer(hostname, port);
+  console.log(packet);
+  packet.open();
+  packet.monitor();
 }
 
 function draw() {}
