dd5c160e06c8be5f0d531df87c7697195bfa059f
Jen Kagan <kaganjd@gmail.com>
2018-09-16 17:27:59 -0400

    Add /sniffer websocket endpoint and runSniffer() function on client side

--- p5.packets-server/views.py
+++ p5.packets-server/views.py
@@ -3,6 +3,23 @@ from aiohttp import web
 from scapy.all import *
 from sniff import get_interface
 
-async def interface_socket(request):
+async def serve_interface(request):
     return web.json_response(get_interface())
+
+async def sniffer_socket(request):
+    ws = web.WebSocketResponse()
+    await ws.prepare(request)
+    
+    async for msg in ws:
+        if msg.type == aiohttp.WSMsgType.TEXT:
+            if msg.data == 'close':
+                await ws.close()
+            else:
+                print(msg.data)
+                await ws.send_str('message from server')
+        elif msg.type == aiohttp.WSMsgType.ERROR:
+            print('ws connection closed with exception %s' %
+                  ws.exception())
+    print('websocket connection closed')
+    return ws
 
