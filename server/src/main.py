04c38a6385b8e792c458955acb8f41b5269ed9d8
Alden Jones <rivendalejones@gmail.com>
2019-09-03 13:58:06 -0400

    getting permission resets to work

--- server/src/main.py
+++ server/src/main.py
@@ -2,7 +2,6 @@ from aiohttp import web
 from routes import setup_routes
 import subprocess
 import config
-import os
 import signal
 
 def run_cmds(*cmds):
@@ -13,39 +12,40 @@ def run_cmds(*cmds):
         cmd_name, exitcode, err = cmds[index], e.returncode, e.output
         print("ERROR RUNNING: '{}' OUTPUT: {}".format(cmd_name, out))
 
-def check_permissions():
-  # TODO: check for other operating systems
-  ls_permissions = config.OSX_LS_BPF_PERMISSIONS
-  permissions = subprocess.run(ls_permissions, shell=True, check=True, stdout=subprocess.PIPE)
-  permissions_str = permissions.stdout.decode()
-  if config.OSX_BPF_PERMISSIONS in permissions_str:
-    try:
-      set_permissions()
-    except:
-      print('Set_permissions failed')
-  else:
-    print('Permissions already set')
+# def check_permissions():
+#   # TODO: check for other operating systems
+#   ls_permissions = config.OSX_LS_BPF_PERMISSIONS
+#   permissions = subprocess.run(ls_permissions, shell=True, check=True, stdout=subprocess.PIPE)
+#   permissions_str = permissions.stdout.decode()
+#   if config.OSX_BPF_PERMISSIONS in permissions_str:
+#     try:
+#       set_permissions()
+#     except:
+#       print('Set_permissions failed')
+#   else:
+#     print('Permissions already set')
 
 def set_permissions():
+  print('Setting permissions, you may need to enter your password...')
   add_permissions = config.OSX_ADD_BPF_PERMISSIONS
   enable_forwarding = config.OSX_ENABLE_FWD
   # enable_firewall = config.OSX_ENABLE_FIREWALL
   run_cmds(add_permissions, enable_forwarding)
+  print('Permissions set')
 
 def restore_permissions():
+  print(' Restoring to default permissions...')
   subtract_permissions = config.OSX_SUBTR_BPF_PERMISSIONS
   disable_forwarding = config.OSX_DISABLE_FWD
   # disable_firewall = config.OSX_DISABLE_FIREWALL
   run_cmds(subtract_permissions, disable_forwarding)
+  print('Permissions restored')
 
-check_permissions()
+
+# check_permissions()
+set_permissions()
 app = web.Application()
-try:
-  setup_routes(app)
-  web.run_app(app)
-# This does not kill the server while the websockets are active
-except KeyboardInterrupt:
-  print('Restoring default permissions...')
-  restore_permissions()
-  # close server via the process ID
-  os.kill(os.getpid(), signal.SIGINT)
+setup_routes(app)
+web.run_app(app)
+restore_permissions()
+
